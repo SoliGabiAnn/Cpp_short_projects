2b42d81715454f55b26cd0ca88248297b8723157
Gabriela Solich <166647049+SoliGabiAnn@users.noreply.github.com>
2026-03-15 13:21:55 +0100

    Delete kody_T08_G5 directory

--- kody_T08_G5/test1_cppcheck.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int arr[5];
-    arr[5] = 10;  // arrayIndexOutOfBounds
-    int *ptr = NULL;
-    *ptr = 42;    // nullPointer
-    int x; printf("%d\n", x);  // uninitializedVariable
-    return 0;
-}
--- kody_T08_G5/test3_sonar.cpp
+++
@@ -1,6 +0,0 @@
-int process(int n) {
-    if (n > 0) { printf("Positive"); }
-    else { printf("Non-positive"); }  // duplikacja
-    if (n > 0) { printf("Positive"); }  // zduplikowany kod, wysoka cyclomatic complexity
-    return n;
-}
